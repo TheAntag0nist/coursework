4dd0c64f791d96831e8dfe7235eade06bdc49f8c
TheAntag0nist <teorus2314@yandex.ru>
2020-09-24 22:57:03 +0300

    modified:   .gitignore 	new file:   CMakeLists.txt 	new file:   build/ALL_BUILD.vcxproj 	new file:   build/ALL_BUILD.vcxproj.filters 	new file:   build/CMakeCache.txt 	new file:   build/CMakeFiles/3.18.3/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/3.18.3/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/3.18.3/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/3.18.3/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/3.18.3/CMakeRCCompiler.cmake 	new file:   build/CMakeFiles/3.18.3/CMakeSystem.cmake 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/CompilerIdC.exe 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/CompilerIdC.vcxproj 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CMakeCCompilerId.obj 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.exe.recipe 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/CL.command.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/CL.read.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/CL.write.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/CompilerIdC.lastbuildstate 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/link.command.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/link.read.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdC/Debug/CompilerIdC.tlog/link.write.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/CompilerIdCXX.exe 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/CompilerIdCXX.vcxproj 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CMakeCXXCompilerId.obj 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.exe.recipe 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/CL.command.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/CL.read.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/CL.write.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/CompilerIdCXX.lastbuildstate 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/link.command.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/link.read.1.tlog 	new file:   build/CMakeFiles/3.18.3/CompilerIdCXX/Debug/CompilerIdCXX.tlog/link.write.1.tlog 	new file:   build/CMakeFiles/3.18.3/VCTargetsPath.txt 	new file:   build/CMakeFiles/3.18.3/VCTargetsPath.vcxproj 	new file:   build/CMakeFiles/3.18.3/x64/Debug/VCTargetsPath.recipe 	new file:   build/CMakeFiles/3.18.3/x64/Debug/VCTargetsPath.tlog/VCTargetsPath.lastbuildstate 	new file:   build/CMakeFiles/9cd06efb4aeb0d62772cc767c0576829/generate.stamp.rule 	new file:   build/CMakeFiles/CMakeOutput.log 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   build/CMakeFiles/generate.stamp 	new file:   build/CMakeFiles/generate.stamp.depend 	new file:   build/CMakeFiles/generate.stamp.list 	new file:   build/Coursework.sln 	new file:   build/ZERO_CHECK.vcxproj 	new file:   build/ZERO_CHECK.vcxproj.filters 	new file:   build/cmake_install.cmake 	new file:   build/dt_cmp.vcxproj 	new file:   build/dt_cmp.vcxproj.filters 	new file:   dt_cmp.exe 	new file:   objects/basic_structures.o 	new file:   objects/compress.o 	new file:   objects/main.o 	new file:   objects/message.o 	deleted:    output.mtf 	deleted:    output.txt 	modified:   script.bat

--- script.bat
+++ script.bat
@@ -3,5 +3,5 @@ move .\message.o .\objects\message.o
 move .\main.o .\objects\main.o
 move compress.o .\objects\compress.o
 move .\basic_structures.o .\objects\basic_structures.o
-gcc .\objects\main.o .\objects\message.o .\objects\basic_structures.o .\objects\compress.o -o data_compression
+gcc .\objects\main.o .\objects\message.o .\objects\basic_structures.o .\objects\compress.o -o dt_cmp
 
